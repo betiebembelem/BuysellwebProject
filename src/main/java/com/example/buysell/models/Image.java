09f97fb8513d2c622537b72af47876be8dc6dddd
bembelem <nikvedd@gmail.com>
2024-07-26 12:53:34 +1100

    Authorization

--- src/main/java/com/example/buysell/models/Image.java
+++ src/main/java/com/example/buysell/models/Image.java
@@ -1,13 +1,12 @@
 package com.example.buysell.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
 @Entity
-@Table (name = "images")
+@Table(name = "images")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,18 +16,12 @@ public class Image {
     private Long id;
 
     private String name;
-
     private String originalFileName;
-
     private Long size;
-
     private String contentType;
-
     private boolean isPreviewImage;
-
     @Lob
     private byte[] bytes;
-
     @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
     private Product product;
 }
